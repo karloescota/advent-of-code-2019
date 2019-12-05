6a71e2a3e619fdd96819af644532a899725f9dfb
Karlo Escota <karlo.escota@gmail.com>
2019-12-05 17:39:18 +0800

    Day01 part 2 total cumulative fuel required

--- lib/advent/day_01.ex
+++ lib/advent/day_01.ex
@@ -6,4 +6,18 @@ defmodule Advent.Day01 do
   def total_fuel_required(masses) do
     Enum.reduce(masses, 0, &(fuel_required(&1) + &2))
   end
+
+  def cummulative_fuel_required(mass) do
+    case fuel_required(mass) do
+      fuel when fuel > 0 ->
+        fuel + cummulative_fuel_required(fuel)
+
+      _ ->
+        0
+    end
+  end
+
+  def total_cummulative_fuel_required(masses) do
+    Enum.reduce(masses, 0, &(cummulative_fuel_required(&1) + &2))
+  end
 end
--- test/advent/day_01.exs
+++ test/advent/day_01.exs
@@ -3,7 +3,7 @@ defmodule AdventTest.Day01Test do
 
   alias Advent.Day01
 
-  test "fuel required sample" do
+  test "fuel required" do
     mass = 12
     assert Day01.fuel_required(mass) == 2
 
@@ -17,8 +17,24 @@ defmodule AdventTest.Day01Test do
     assert Day01.fuel_required(mass) == 33583
   end
 
-  test "fuel required input" do
+  test "total fuel required" do
     masses = [12, 14, 1969]
     assert Day01.total_fuel_required(masses) == 658
+  end
+
+  test "cummulative fuel required" do
+    mass = 14
+    assert Day01.cummulative_fuel_required(mass) == 2
+
+    mass = 1969
+    assert Day01.cummulative_fuel_required(mass) == 966
+
+    mass = 100_756
+    assert Day01.cummulative_fuel_required(mass) == 50346
+  end
+
+  test "total cummulative fuel required" do
+    masses = [14, 1969, 100_756]
+    assert Day01.total_cummulative_fuel_required(masses) == 51314
   end
 end
